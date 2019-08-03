beebb77dc3955bd414c2a4a1db3963a14895dd80
Brendan <bpdavitt@gmail.com>
2019-08-03 14:56:52 -0500

    Implement change/submit handlers for input field; Input requires a number <= pins remaining

--- challenge_3/client/App.jsx
+++ challenge_3/client/App.jsx
@@ -6,17 +6,41 @@ import Selector from './components/Selector.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      pinsLeft: 10,
+      pinsHit: 0
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    if (e.key === 'Enter') {
+      this.handleSubmit()
+    } else {
+      console.log(e.target.value)
+      this.setState({ pinsHit: e.target.value })
+    }
+  }
+
+  handleSubmit(e) {
+    console.log('Tried to submit a score')
+    if(this.state.pinsHit > this.state.pinsLeft) {
+      alert(`Please enter a valid score; must be <= ${this.state.pinsLeft}`)
+    } else {
+      // Log result, check to see if new frame, etc => helper fx
+    }
   }
 
   render() {
     return (
       <>
         <div>Lebowski Lanes Open For Business</div>
-        <Selector></Selector>
+        <Selector handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}></Selector>
       </>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
--- challenge_3/client/components/Selector.jsx
+++ challenge_3/client/components/Selector.jsx
@@ -4,8 +4,13 @@ const Selector = (props) => {
   return (
     <>
       <div id="select-container">
-        <input type="number" placeholder="How many pins did you just wreck?" id="pin-selector"></input>
-        <button id="pin-submit">Submit</button>
+        <input type="number"
+          placeholder="How many pins did you just wreck?"
+          id="pin-selector"
+          max={10}
+          min={0}
+          onKeyUp={(e)=>{props.handleChange(e)}}></input>
+        <button id="pin-submit" onClick={(e)=>{props.handleSubmit(e)}}>Submit</button>
       </div>
     </>
   )
