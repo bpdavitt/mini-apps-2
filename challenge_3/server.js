6e85cd244fae003a45b28c282af1f210f6225cec
Brendan <bpdavitt@gmail.com>
2019-08-03 13:51:37 -0500

    Initialize package, webpack, server, hello World HTML

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.send("OVER THE LINE")
+})
+
+app.listen(PORT, ()=> console.log('The dude abides on port', PORT))
