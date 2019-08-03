6d96a9960b3fb3859a32fb34d83604976fb519d1
Brendan <bpdavitt@gmail.com>
2019-08-03 10:38:43 -0500

    Initialize server, serving up Hello World

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.send('Get yo bitcoins');
+});
+
+app.listen(PORT, () => console.log('Crypto Server running on port', PORT))
