52f546ccb7d82f815179dde9a24333819aff12f9
Brendan <bpdavitt@gmail.com>
2019-08-02 19:21:28 -0500

    Pagination implemented;  Further UI optimization required; Need to decrease max pages when blank

--- challenge_1/client/Components/Events.jsx
+++ challenge_1/client/Components/Events.jsx
@@ -5,16 +5,40 @@ import ReactPaginate from 'react-paginate';
 class Events extends React.Component {
   constructor(props) {
     super(props);
-    this.displayed = props.events.slice(0,10);
+    this.state = {
+      displayed: props.events.slice(0,10)
+    }
+    this.perPage = 10;
+    this.handlePageClick = this.handlePageClick.bind(this);
+  }
+
+  handlePageClick (data) {
+    let selected = data.selected;
+    let offset = Math.ceil(selected * this.perPage);
+
+    this.setState({displayed: this.props.events.slice(offset, offset + 10)});
   }
 
   render () {
     return (
-      <>
-        {this.displayed.map((event, i) => {
+      <div className="eventList">
+        {this.state.displayed.map((event, i) => {
           return <Event event={event} key={'event:' + i}></Event>
         })}
-      </>
+        <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={this.perPage}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
+      </div>
     )
   }
 }
